ccc5d5acd5e0783c75eb97eb39dd664eede82082
Christian <Christian@WORKSTATION>
2016-11-01 10:23:07 +0100

    Add project files.

---
+++ Lommeregner/Skynet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lommeregner
+{
+    class Skynet
+    {
+        public double Plus(double tal1, double tal2)
+        {
+            double value = 0;
+            value = tal1 + tal2;
+            return value;
+        }
+    }
+}
