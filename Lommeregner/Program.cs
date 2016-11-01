95b772f65f28471682700da57a41d44efeca8b47
Christian <Christian@WORKSTATION>
2016-11-01 12:36:55 +0100

    Har lavet gange og divider  og ændret int til double

--- Lommeregner/Program.cs
+++ Lommeregner/Program.cs
@@ -10,40 +10,56 @@ namespace Lommeregner
     {
         static void Main(string[] args)
         {
-            int num1 = 0;
-            int num2 = 0;
-            int funk1 = 0;
+            double num1 = 0;
+            double num2 = 0;
             int stop = 1;
             while (stop != 0)
             {
 
 
-                Console.WriteLine("Tas 1 for + og 2 for - og 3 for at slutte");
+                Console.WriteLine("Tas + for plus, - for minus, * for gange, / for divider og 1 for at slutte");
                 string isit = Console.ReadLine();
-                Int32.TryParse(isit, out funk1);
-                if (funk1 == 1)
+                if (isit == "+")
                 {
                     Console.WriteLine("Tast tal");
                     string input1 = Console.ReadLine();
                     string input2 = Console.ReadLine();
-                    //string input3 = Console.ReadLine();
-                    Int32.TryParse(input1, out num1);
-                    Int32.TryParse(input2, out num2);
-                    int plus = num1 + num2;
+                    Double.TryParse(input1, out num1);
+                    Double.TryParse(input2, out num2);
+                    double plus = num1 + num2;
                     Console.WriteLine("Svar er " + plus);
                 }
-                if (funk1 == 2)
+                else if (isit == "-")
                 {
                     Console.WriteLine("Tast tal");
                     string input3 = Console.ReadLine();
                     string input4 = Console.ReadLine();
-                    //string input3 = Console.ReadLine();
-                    Int32.TryParse(input3, out num1);
-                    Int32.TryParse(input4, out num2);
-                    int minus = num1 - num2;
+                    Double.TryParse(input3, out num1);
+                    Double.TryParse(input4, out num2);
+                    double minus = num1 - num2;
                     Console.WriteLine("Svar er " + minus);
                 }
-                if (funk1 == 3)
+                else if (isit == "*")
+                {
+                    Console.WriteLine("Tast tal");
+                    string input3 = Console.ReadLine();
+                    string input4 = Console.ReadLine();
+                    Double.TryParse(input3, out num1);
+                    Double.TryParse(input4, out num2);
+                    double gange = num1 * num2;
+                    Console.WriteLine("Svar er " + gange);
+                }
+                else if (isit == "/")
+                {
+                    Console.WriteLine("Tast tal");
+                    string input3 = Console.ReadLine();
+                    string input4 = Console.ReadLine();
+                    Double.TryParse(input3, out num1);
+                    Double.TryParse(input4, out num2);
+                    double divider = num1 / num2;
+                    Console.WriteLine("Svar er " + divider);
+                }
+                else if (isit == "1")
                 {
                     stop = 0;
                     Console.WriteLine("Program slutter");
@@ -55,5 +71,6 @@ namespace Lommeregner
             }
 
         }
+        
     }
 }
